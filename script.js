37c00e6fa933e8db13131d1e75208a8d4683527f
utahbug <ksrid@icloud.com>
2026-07-28 11:55:09 -0600

    Make PDF touch taps turn pages directly

--- script.js
+++ script.js
@@ -4415,10 +4415,13 @@ function handlePdfTapZoneClick(event, direction) {
     return;
   }
 
+  performPdfTapZoneAction(event.clientY, direction);
+}
+
+function performPdfTapZoneAction(clientY, direction) {
   const stageBox = el.pdfStage.getBoundingClientRect();
-  const tappedTopQuarter = event.clientY <= stageBox.top + stageBox.height / 4;
+  const tappedTopQuarter = clientY <= stageBox.top + stageBox.height / 4;
   if (tappedTopQuarter) {
-    event.preventDefault();
     if (direction === "previous") {
       firstPdfPage();
     } else {
@@ -4516,6 +4519,18 @@ function handlePdfTouchEnd(event) {
       previousPdfPage();
     }
     return;
+  }
+
+  if (state.currentPdf.touchMode === "tap" && !moved) {
+    const tapZone = event.target.closest(".pdf-tap-zone");
+    if (tapZone) {
+      event.preventDefault();
+      const direction = tapZone === el.pdfTapLeft ? "previous" : "next";
+      finishPdfGesture();
+      performPdfTapZoneAction(touch.clientY, direction);
+      suppressNextPdfClick();
+      return;
+    }
   }
 
   finishPdfGesture();
