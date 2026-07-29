f3ee3282c1b429e8373552eabe70eaf7a6d61ba5
utahbug <ksrid@icloud.com>
2026-07-29 14:56:47 -0600

    Add visual Fun Stuff sound picker

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v43";
+const CACHE_NAME = "musicdocs-shell-v44";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-25",
+  "./styles.css?v=musicdocs-26",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-31",
+  "./script.js?v=musicdocs-32",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
