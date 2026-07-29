fcba919ab18b0b33ef23002fe365adb9fb3e574e
utahbug <ksrid@icloud.com>
2026-07-29 09:41:56 -0600

    Explain Follow behavior on Lists page

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v21";
+const CACHE_NAME = "musicdocs-shell-v22";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-10",
+  "./styles.css?v=musicdocs-11",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-19",
   "./library.json",
