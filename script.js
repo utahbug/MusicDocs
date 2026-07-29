bd28edfa69abc3e69ff30661e8e26e11db8b2d28
utahbug <ksrid@icloud.com>
2026-07-29 09:07:23 -0600

    Start with all lists closed

--- script.js
+++ script.js
@@ -1284,7 +1284,7 @@ function setupInitialSelections() {
   populateSelect(el.listSelect, state.lists || []);
   const defaultList = state.lists[0];
   state.activeListId = defaultList?.id || "";
-  state.expandedListIds = state.activeListId ? [state.activeListId] : [];
+  state.expandedListIds = [];
   el.listSelect.value = state.activeListId;
 }
 
