460afd31b76ee7973d2441322dbd3ca599a10886
utahbug <ksrid@icloud.com>
2026-07-28 21:37:17 -0600

    Rename list-following toggle

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v14";
+const CACHE_NAME = "musicdocs-shell-v15";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=musicdocs-7",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-13",
+  "./script.js?v=musicdocs-14",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
