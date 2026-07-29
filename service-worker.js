0325b91cca759f339f5fe8265b7c2ce93fcb5f9a
utahbug <ksrid@icloud.com>
2026-07-28 22:13:26 -0600

    Add playful metronome sounds

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v19";
+const CACHE_NAME = "musicdocs-shell-v20";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=musicdocs-9",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-17",
+  "./script.js?v=musicdocs-18",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
