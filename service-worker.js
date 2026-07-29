c9ea7074af418b95c7f02ddd4048bb1e60e98a17
utahbug <ksrid@icloud.com>
2026-07-29 10:16:18 -0600

    Refine Note Arc spacing and shape

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v24";
+const CACHE_NAME = "musicdocs-shell-v25";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-12",
+  "./styles.css?v=musicdocs-13",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-21",
   "./library.json",
