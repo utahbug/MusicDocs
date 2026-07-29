99c7852f4bc1da4d4da5364c6d4e766fe60f9543
utahbug <ksrid@icloud.com>
2026-07-29 16:56:30 -0600

    Prevent page movement while playing notes

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v48";
+const CACHE_NAME = "musicdocs-shell-v49";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-28",
+  "./styles.css?v=musicdocs-29",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-36",
   "./library.json",
