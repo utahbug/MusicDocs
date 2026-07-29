ae6fed755b4c377d4de5392314bff596ace3af69
utahbug <ksrid@icloud.com>
2026-07-29 12:14:15 -0600

    Align Tap Zones close notice right

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v40";
+const CACHE_NAME = "musicdocs-shell-v41";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-22",
+  "./styles.css?v=musicdocs-23",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-29",
   "./library.json",
