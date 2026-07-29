8907fcb6c02e043e465d8e007cdb72a9bdaf1b96
utahbug <ksrid@icloud.com>
2026-07-29 11:40:27 -0600

    Add Music Playground games and chord guide

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v31";
+const CACHE_NAME = "musicdocs-shell-v33";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-16",
+  "./styles.css?v=musicdocs-18",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-25",
+  "./script.js?v=musicdocs-27",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
