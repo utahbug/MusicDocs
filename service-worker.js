d51a08b8563057bdca4a2cfdf59668cc10166fa2
utahbug <ksrid@icloud.com>
2026-07-30 10:30:03 -0600

    Use hands-over-ears icon for Just sound

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v103";
+const CACHE_NAME = "musicdocs-shell-v104";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-68",
+  "./styles.css?v=musicdocs-69",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-68",
   "./library.json",
