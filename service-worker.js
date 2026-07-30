039ddd2f78f6e22e3ba5a5147140a7de6e0448c8
utahbug <ksrid@icloud.com>
2026-07-30 12:10:00 -0600

    Refresh Wave and Pop notes icons

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v122";
+const CACHE_NAME = "musicdocs-shell-v124";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-79",
+  "./styles.css?v=musicdocs-81",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-80",
   "./library.json",
