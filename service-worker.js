d2eb4c36ae448fb91d2828d6142c8447ba57a5e6
utahbug <ksrid@icloud.com>
2026-07-30 11:23:04 -0600

    Smooth chord playback sustain

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v112";
+const CACHE_NAME = "musicdocs-shell-v113";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=musicdocs-74",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-73",
+  "./script.js?v=musicdocs-74",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
