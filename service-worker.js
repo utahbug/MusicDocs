3e6a6227dddb8fd107d690c2dae9eb05546098e7
utahbug <ksrid@icloud.com>
2026-07-30 11:44:07 -0600

    Replace Just sound activity icon

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v115";
+const CACHE_NAME = "musicdocs-shell-v116";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-74",
+  "./styles.css?v=musicdocs-75",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-76",
   "./library.json",
