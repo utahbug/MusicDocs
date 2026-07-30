e365dde914bfef218fafbb53881a9e5c85595e1d
utahbug <ksrid@icloud.com>
2026-07-30 10:58:55 -0600

    Add scales and keyboard transposition

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v109";
+const CACHE_NAME = "musicdocs-shell-v110";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-73",
+  "./styles.css?v=musicdocs-74",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-71",
+  "./script.js?v=musicdocs-72",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
