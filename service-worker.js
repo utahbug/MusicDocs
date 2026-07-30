a5da32abb5abc2d792811f6a04bee9140d9c441f
utahbug <ksrid@icloud.com>
2026-07-29 18:03:15 -0600

    Add Classical guitar sound

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v50";
+const CACHE_NAME = "musicdocs-shell-v51";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=musicdocs-30",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-37",
+  "./script.js?v=musicdocs-38",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
