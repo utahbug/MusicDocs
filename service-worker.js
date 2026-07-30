287e8b8385ed8549aa3a282933e9c9db87c419f2
utahbug <ksrid@icloud.com>
2026-07-30 08:23:56 -0600

    Complete Sidetracked octave

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v85";
+const CACHE_NAME = "musicdocs-shell-v86";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=musicdocs-53",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-55",
+  "./script.js?v=musicdocs-56",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
