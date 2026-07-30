d5f7f7579d136672c7e85140c5db2cf886e65085
utahbug <ksrid@icloud.com>
2026-07-30 10:21:24 -0600

    Give Just Sound a broken-note icon

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v99";
+const CACHE_NAME = "musicdocs-shell-v100";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-65",
+  "./styles.css?v=musicdocs-66",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-65",
   "./library.json",
