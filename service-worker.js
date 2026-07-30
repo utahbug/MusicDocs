803ac077af7fe096be2ded13625360ffa8534d42
utahbug <ksrid@icloud.com>
2026-07-30 10:40:22 -0600

    Refine keyboard guides and Just Sound layout

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v107";
+const CACHE_NAME = "musicdocs-shell-v108";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-71",
+  "./styles.css?v=musicdocs-72",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-69",
+  "./script.js?v=musicdocs-71",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
