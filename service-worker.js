2e2fa256f89517a487e0829b85fc0a7bc21849db
utahbug <ksrid@icloud.com>
2026-07-30 13:29:12 -0600

    Rename and center Jazzy activity

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v125";
+const CACHE_NAME = "musicdocs-shell-v127";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=musicdocs-82",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-80",
+  "./script.js?v=musicdocs-81",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
