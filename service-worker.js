a82c233f600d3fa880c4588d458e31f359a88b2a
utahbug <ksrid@icloud.com>
2026-07-30 10:35:43 -0600

    Compact chord guide fields

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v106";
+const CACHE_NAME = "musicdocs-shell-v107";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-70",
+  "./styles.css?v=musicdocs-71",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-69",
   "./library.json",
