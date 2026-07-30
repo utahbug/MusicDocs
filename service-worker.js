0a90cf170bd4e53a421ed2eeb173fa6e26a2a887
utahbug <ksrid@icloud.com>
2026-07-30 13:27:45 -0600

    Give Wave a curved keyboard icon

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v124";
+const CACHE_NAME = "musicdocs-shell-v125";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-81",
+  "./styles.css?v=musicdocs-82",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-80",
   "./library.json",
