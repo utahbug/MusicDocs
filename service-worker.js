2392ee97391c1eb1a4abc455dc8a837cbd862c16
utahbug <ksrid@icloud.com>
2026-07-30 10:32:39 -0600

    Add musical surprises to Pop Notes

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v104";
+const CACHE_NAME = "musicdocs-shell-v105";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=musicdocs-69",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-68",
+  "./script.js?v=musicdocs-69",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
