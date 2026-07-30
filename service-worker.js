8364c497a688c9b5fc9882b75fb750e8079162c1
utahbug <ksrid@icloud.com>
2026-07-29 19:19:32 -0600

    Indent Follow note marker

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v61";
+const CACHE_NAME = "musicdocs-shell-v62";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-38",
+  "./styles.css?v=musicdocs-39",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-41",
   "./library.json",
