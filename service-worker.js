6ce2382b78ffe06c5d3b9ea4bbca7307b1ab869e
utahbug <ksrid@icloud.com>
2026-07-29 19:07:36 -0600

    Use regular weight for library titles

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v57";
+const CACHE_NAME = "musicdocs-shell-v58";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-35",
+  "./styles.css?v=musicdocs-36",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-41",
   "./library.json",
