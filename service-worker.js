b3143e0979b0386a662af76652f81b6371ead578
utahbug <ksrid@icloud.com>
2026-07-30 13:37:17 -0600

    Build playable Jazzy keyboard ribbons

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v128";
+const CACHE_NAME = "musicdocs-shell-v129";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-83",
+  "./styles.css?v=musicdocs-84",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-82",
+  "./script.js?v=musicdocs-83",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
