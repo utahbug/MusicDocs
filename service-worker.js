284f5b04944cdaaf458c4b21b3101eafd00ceba7
utahbug <ksrid@icloud.com>
2026-07-30 08:11:49 -0600

    Build Sidetracked activities

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v84";
+const CACHE_NAME = "musicdocs-shell-v85";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-52",
+  "./styles.css?v=musicdocs-53",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-54",
+  "./script.js?v=musicdocs-55",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
