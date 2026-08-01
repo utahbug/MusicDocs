ba239e8307207c1d9dc72fe5d3bd3900602836cd
utahbug <ksrid@icloud.com>
2026-08-01 16:13:03 -0600

    Fix Key Order note labels

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v146";
+const CACHE_NAME = "musicdocs-shell-v148";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-96",
+  "./styles.css?v=musicdocs-98",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-93",
+  "./script.js?v=musicdocs-94",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
