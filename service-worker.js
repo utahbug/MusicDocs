a1f486c36317524bf04129ac6dc9570752e0d175
utahbug <ksrid@icloud.com>
2026-08-01 16:37:22 -0600

    Play Pop Notes through jazz scale

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "musicdocs-shell-v152";
+const CACHE_NAME = "musicdocs-shell-v153";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=musicdocs-101",
   "./lyrics-cards.js?v=musicdocs-2",
-  "./script.js?v=musicdocs-95",
+  "./script.js?v=musicdocs-96",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
