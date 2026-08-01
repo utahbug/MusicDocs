96efe27c2b3b7a127996e1915f87c147e43cdf77
utahbug <ksrid@icloud.com>
2026-08-01 15:21:45 -0600

    Show list controls only when selected

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "musicdocs-shell-v140";
+const CACHE_NAME = "musicdocs-shell-v141";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=musicdocs-92",
+  "./styles.css?v=musicdocs-93",
   "./lyrics-cards.js?v=musicdocs-2",
   "./script.js?v=musicdocs-91",
   "./library.json",
